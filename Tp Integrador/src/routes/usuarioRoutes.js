e2eacd1753919ec4599350a33a609107c02ccb12
mncrmn2009 <mnc.rmn.2009@gmail.com>
2025-11-07 12:23:09 -0300

    usuarioRoutes, authmiddleware

--- Tp Integrador/src/routes/usuarioRoutes.js
+++ Tp Integrador/src/routes/usuarioRoutes.js
@@ -1,18 +1,22 @@
 import express from "express";
 import { listarUsuarios, obtenerUsuarioPorId, crearUsuario, eliminarUsuario } from "../controllers/usuarioController.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 //GET /api/usuarios -> listar todos los usuarios
-router.get("/", listarUsuarios);
+//router.get("/", listarUsuarios);
+router.get("/", authMiddleware, listarUsuarios);
 
 // GET /api/usuarios/:id → detalle de un usuario
-router.get("/:id", obtenerUsuarioPorId);
+//router.get("/:id", obtenerUsuarioPorId);
+router.get("/:id", authMiddleware, obtenerUsuarioPorId);
 
 // POST /api/usuarios → registrar usuario
 router.post("/", crearUsuario);
 
 // DELETE /api/usuarios/:id → eliminar usuario y su carrito
-router.delete("/:id", eliminarUsuario);
+//router.delete("/:id", eliminarUsuario);
+router.delete("/:id", authMiddleware, eliminarUsuario);
 
 export default router;
